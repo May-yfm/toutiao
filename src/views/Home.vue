e82987faa5217eb034b8a14e48660989554250a3
yfm <3032912803@qq.com>
2020-07-30 18:57:08 +0800

    首页 导航条

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
+<!-- 主页组件 -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class='tt-box'>
+  <!-- 主页头部 -->
+    <div class="tt-home-header">
+        <h2>头条</h2>
+    </div>
+  <!-- 主页内容 -->
+  <div class="tt-home-content">
+      <div class='content-left'>
+          <Navigator></Navigator>
+      </div>
+      <div class='content-middle'>中间</div>
+      <div class='content-right'>右边</div>
   </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+//引入导航navigator组件
+import Navigator from "../components/Navigator";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+//import引入的组件需要注入到对象中才能使用
+components: {
+  Navigator
+},
+data() {
+//这里存放数据
+return {
+
+};
+},
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+beforeCreate() {}, //生命周期 - 创建之前
+beforeMount() {}, //生命周期 - 挂载之前
+beforeUpdate() {}, //生命周期 - 更新之前
+updated() {}, //生命周期 - 更新之后
+beforeDestroy() {}, //生命周期 - 销毁之前
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
+<style  lang='less' scoped>
+  * {margin: 0;padding: 0;}
+  .tt-home-header {
+    width: 100%;
+    background: black;
+    height: 30px;
+    h2 {
+      color: white;
+      font-weight:100;
+      font-size: 18px;
+      line-height: 30px;
+      padding-left: 5px;
+    }
+  }
+  .tt-home-content {
+    width: 80vw;
+    margin: 0 auto;
+    display: flex;
+    background: pink;
+    .content-left {
+      flex: 1;
+      background: yellow;
+    }
+
+    .content-middle {
+      flex: 3;
+      background: orange;
+    }
+
+    .content-right {
+      flex: 2;
+    }
+  }
+</style>
